ec92efff3cc9b63525d532e79c41e60b213496c4
Marcello <marcello.crowe@yahoo.com>
2023-11-24 19:49:36 -0700

    Finished

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // code your solution here
+function saturdayFun(activity) {
+    let saturdayActivity;
+  
+    if (activity) {
+      saturdayActivity = activity;
+    } else {
+      saturdayActivity = "roller-skate";
+    }
+  
+    return `This Saturday, I want to ${saturdayActivity}!`;
+  }
+  const mondayWork = function(activity) {
+    if (activity) {
+      return `This Monday, I will ${activity}.`;
+    } else {
+      return "This Monday, I will go to the office.";
+    }
+  };
+  function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    }
+  }
